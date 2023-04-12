088837451b7e21120bf5161f8bb518f390e697a4
Arslan Shahab <arslanshahab@gmail.com>
2023-04-12 13:01:38 +0500

    applied inline styling

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { allMovies } from "../data/movies";
 
 export const MoviesList = () => {
-  const [movies, setMovies] = useState([1, 232, 23]);
+  //   const [movies, setMovies] = useState([1, 232, 23]);
+
+  // CONDITIONAL RENDERING
 
   //   if (movies.length === 0) {
   //     return <i>No Movies Found...</i>;
@@ -9,9 +12,28 @@ export const MoviesList = () => {
   //     return <h1>Here is the movies list: 1,2,3,23,2,</h1>;
   //   }
 
-  return movies.length === 0 ? (
-    <i>No Movies Found...</i>
-  ) : (
-    <h1>Here is the movies list: 1,2,3,23,2,</h1>
+  return (
+    <React.Fragment>
+      <h1>All Movies</h1>
+      <div className="movie-wrapper">
+        {allMovies.map((movie) => {
+          return (
+            <div className="movie-item">
+              <h4 style={{
+                  color: 'orangered',
+                  fontSize: '32px',
+                  border: '1px solid #ddd',
+                  borderRadius: '8px',
+                  backgroundColor: 'black',
+                  textAlign: 'center'
+              }}>{movie.title}</h4>
+              <p>{movie.director}</p>
+              <p>Rating: {movie.rating}</p>
+              <button>View Details</button>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 };
